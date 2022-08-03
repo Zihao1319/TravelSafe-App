31eb444a95ec179e624ff59b50de2d478e7d0a04
Zihao1319 <zihao1319@gmail.com>
2022-08-04 00:01:49 +0800

    initialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtuQHsHFdyVU3HCMw_U9hhbGvTg797RkU",
+  authDomain: "travel-app-c53d2.firebaseapp.com",
+  projectId: "travel-app-c53d2",
+  storageBucket: "travel-app-c53d2.appspot.com",
+  messagingSenderId: "440305880358",
+  appId: "1:440305880358:web:2def0cce08e836c9d005e7",
+  databaseURL:
+    "https://travel-app-c53d2-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
