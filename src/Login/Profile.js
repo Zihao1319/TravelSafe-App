b53ee889aa16e6ad597d007856c3d51221fd8af0
Zihao1319 <zihao1319@gmail.com>
2022-08-30 00:57:58 +0800

    firebease realtime database err

--- src/Login/Profile.js
+++ src/Login/Profile.js
@@ -7,7 +7,8 @@ import {
   onValue,
   update,
   set,
-  setValue
+  child,
+  setValue,
 } from "firebase/database";
 import {
   getStorage,
@@ -26,9 +27,8 @@ import Box from "@mui/material/Box";
 
 const Profile = () => {
   const { user, setUser } = useUserContext();
-  const [imgFile, setImgFile] = useState({
-    file: undefined,
-  });
+  const [imgFile, setImgFile] = useState();
+  const [imgURL, setImgURL] = useState();
 
   //   const auth = getAuth();
   //   const userInfo = auth.currentUser;
@@ -41,31 +41,71 @@ const Profile = () => {
   //   const lastName = user.lastName;
   //   const email = user.email;
 
-  const handleImgChange = async (e) => {
+  const handleImgChange = (e) => {
     // e.preventDefault();
     const filePath = e.target.files[0];
+    console.log(filePath);
     // const isImg = e.target.files[0]["type"].split("/")[0] === "image";
     // console.log(isImg)
-    setImgFile({ file: filePath });
-    const storage = getStorage();
-    const fileRef = refStorage(storage, `userPictureFolder/${user.uid}`);
-    console.log(imgFile.file);
+    setImgFile(filePath);
+    console.log(imgFile);
+  };
 
-    await uploadBytes(fileRef, imgFile.file);
-    const imgDownloadUrl = await getDownloadURL(fileRef);
-    console.log(imgFile, imgDownloadUrl);
-
-
-    // setUser ((prev) => ({...prev}, {photo: imgDownloadUrl}))
-    console.log(user)
+  const handleImgUpload = async (e) => {
+    e.preventDefault();
 
     const database = getDatabase();
-    const userRef = refDatabase(database, `userInfo/${user.uid}/photoURL`);
-    console.log(userRef)
-    update(userRef, {imgDownloadUrl})
+
+    if (imgFile) {
+      console.log("start of upload");
+      const storage = getStorage();
+      const fileRef = refStorage(storage, `userPictureFolder/${user.uid}`);
+      await uploadBytes(fileRef, imgFile);
+      const imgDownloadUrl = await getDownloadURL(fileRef);
+      console.log(imgDownloadUrl);
+      setImgURL(imgDownloadUrl);
+
+      // const userRef = refDatabase(database, "userInfo/" + `${user.uid}`);
+      // console.log(child(refDatabase(database), `${user.uid}`).key);
+      // console.log(
+      //   child(refDatabase(database), `userInfo/${user.uid}/photoURL`).key
+      // );
+
+      const ref = child(refDatabase(database), `/userInfo/${user.uid}`);
+      const updatedURL = {
+        photoURL: imgDownloadUrl,
+      };
+
+      // const userRef = child(refDatabase(database), `userInfo`);
+      // console.log(userRef);
+      // update(userRef, {photo}
+
+      // console.log(userRef, child(database));
+      // update(userRef, { photoURL: imgDownloadUrl });
+    }
+
+    // fileRef.on("state_changed",
+    // (snapshot) => {
+    //   console.log(snapshot)
+    // }, (err) => {
+    //   console.log(err)
+    // }, () => {
+
+    // }
+    // )
+
+    // const fileRef = refStorage(storage, `userPictureFolder/` + `${user.uid}`);
+    // console.log(imgFile.file);
+
+    // // setUser ((prev) => ({...prev}, {photo: imgDownloadUrl}))
+    // console.log(user);
+
+    // const database = getDatabase();
+    // const userRef = refDatabase(database, `userInfo/${user.uid}`);
+    // console.log(userRef);
+    // update(userRef, { photoURL: imgDownloadUrl });
 
     // console.log(photoRef)
-
   };
 
   return (
@@ -80,18 +120,27 @@ const Profile = () => {
       </Typography>
       <Avatar
         alt="Say cheese!"
-        src={user.photo ? user.photo : "/broken-image.jpg"}
+        // src={user.photo ? user.photo : "/broken-image.jpg"}
+        src={imgURL ? imgURL : "/broken-image.jpg"}
         sx={{ width: 100, height: 100 }}
       />
-      <Button size="small" sx={{ mx: "auto" }} component="label">
-        <input
-          type="file"
-          hidden
-          accept="image/*"
-          name="image"
-          onClick={handleImgChange}
-        />
+      <Button
+        size="small"
+        sx={{ mx: "auto" }}
+        component="label"
+        onChange={handleImgChange}
+      >
+        <input type="file" accept="image/*" name="image" hidden />
         Change profile picture
+      </Button>
+      <Button
+        size="small"
+        sx={{ mx: "auto" }}
+        component="label"
+        onClick={handleImgUpload}
+        disabled={!imgFile}
+      >
+        Upload
       </Button>
       <CardContent>
         {/* <Typography gutterBottom variant="h5" component="div">
